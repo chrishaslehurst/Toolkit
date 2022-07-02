abb341348527eb430c69227e0d55bc23b8cb5672
Chris Haslehurst <chrishaslehurst@gmail.com>
2022-07-02 20:13:15 +0100

    Integrated Imgui

--- Toolkit/include/Rendering/Window.h
+++ Toolkit/include/Rendering/Window.h
@@ -19,6 +19,8 @@ namespace toolkit {
 		Mesh square;
 		Material mat;
 		GLFWwindow* window = nullptr;
+
+		bool showDemoWindow = true;
 	};
 
 	void FrameBufferSizeCallback(GLFWwindow* window, s32 width, s32 height);
