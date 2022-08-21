aa35a245d780cc4872e34ee52fa1053bc88b13d3
Chris Haslehurst <chrishaslehurst@gmail.com>
2022-08-21 22:43:49 +0100

    Added constructors for vector 2 and 3s, added vec3 cross product.

--- Toolkit/include/Math/Vector.h
+++ Toolkit/include/Math/Vector.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include <type_traits>
 
 namespace toolkit
 {
@@ -8,14 +9,48 @@ namespace toolkit
 	{
 		namespace vec 
 		{
-			template <int> struct Members { };
-			template <> struct Members<2> { f32 x = 0.f; f32 y = 0.f; };
-			template <> struct Members<3> { f32 x = 0.f; f32 y = 0.f; f32 z = 0.f; };
+			template <int> 
+			struct Members 
+			{ };
+
+			template <> 
+			struct Members<2> 
+			{ 
+				f32 x = 0.f; 
+				f32 y = 0.f; 
+				Members() = default; 
+				Members(f32 inX, f32 inY) :x(inX), y(inY) {} 
+			};
+			
+			template <> 
+			struct Members<3> 
+			{ 
+				f32 x = 0.f; 
+				f32 y = 0.f; 
+				f32 z = 0.f; 
+				Members() = default; 
+				Members(f32 inX, f32 inY, f32 inZ) :x(inX), y(inY), z(inZ) {}
+			};
 		}
 
 		template<int T>
 		struct Vector : vec::Members<T>
 		{
+			using Mems = vec::Members<T>;
+			Vector() = default;
+
+			Vector(f32 inX, f32 inY)
+				: Mems(inX, inY)
+			{
+				static_assert(T == 2, "Wrong Constructor Used");
+			}
+
+			Vector(f32 inX, f32 inY, f32 inZ)
+				: Mems(inX, inY, inZ)
+			{
+				static_assert(T == 3, "Wrong Constructor Used");
+			}
+
 			static_assert(T == 3 || T == 2, "Only supports 2 or 3 elements");
 		};
 
@@ -58,6 +93,15 @@ namespace toolkit
 			else if constexpr (T == 3)
 			{
 				return (lhs.x * rhs.x) + (lhs.y * rhs.y) + (lhs.z * rhs.z);
+			}
+		}
+
+		template<int T>
+		constexpr Vector<T> CrossProduct(const Vector<T>& lhs, const Vector<T>& rhs)
+		{
+			if constexpr (T == 3)
+			{
+				return Vector<T>(((lhs.y * rhs.z) - (lhs.z * rhs.y)), ((lhs.z * rhs.x) - (lhs.x * rhs.z)), ((lhs.x * rhs.y) - (lhs.y * rhs.x)));
 			}
 		}
 	}
--- Toolkit/tests/MathTests.cpp
+++ Toolkit/tests/MathTests.cpp
@@ -6,20 +6,16 @@
 
 TEST_CASE("Vector addition works", "[math]") 
 {
-	toolkit::math::Vector2 vec2_1;
+	toolkit::math::Vector2 vec2_1 {1.f, 2.f};
 	toolkit::math::Vector2 vec2_2;
-	toolkit::math::Vector3 vec3_1;
+	toolkit::math::Vector3 vec3_1 {5.f, 0.f, 3.f};
 	toolkit::math::Vector3 vec3_2;
-
-	vec3_1.x = 5.f;
-	vec3_1.y = 0.f;
-	vec3_1.z = 3.f;
 
 	vec3_2.x = 0.f;
 	vec3_2.y = 4.f;
 	vec3_2.z = 19.f;
 	
-	REQUIRE(vec2_1.x == 0.f);
+	REQUIRE(vec2_1.x == 1.f);
 
 	toolkit::math::Vector<3> result;
 	result = vec3_1 + vec3_2;
@@ -48,4 +44,24 @@ TEST_CASE("Vector Dot Product Works", "[math]")
 	f32 result = toolkit::math::DotProduct(vec3_1, vec3_2);
 
 	REQUIRE(result == -3.f);
+}
+
+TEST_CASE("Vector Cross Product Works", "[math]")
+{
+	toolkit::math::Vector<2> vec2_1;
+	toolkit::math::Vector<2> vec2_2;
+	toolkit::math::Vector<3> vec3_1;
+	toolkit::math::Vector<3> vec3_2;
+
+	vec3_1.x = 1.f;
+	vec3_1.y = 107.f;
+	vec3_1.z = 20.f;
+
+	vec3_2.x = 1.f;
+	vec3_2.y = -1.f;
+	vec3_2.z = -1.f;
+
+	toolkit::math::Vector3 result = toolkit::math::CrossProduct(vec3_1, vec3_2);
+	//TODO write an actual test
+	REQUIRE(result.x == 0.f);
 }
